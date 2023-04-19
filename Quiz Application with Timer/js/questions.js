aba46b8132008d559285ad09874e87157739d979
Isaacbayo <43371623+izkbayo35@users.noreply.github.com>
2023-04-19 11:42:03 +0100

    Add files via upload

---
+++ Quiz Application with Timer/js/questions.js
@@ -0,0 +1,119 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the full form of “AI”",
+    answer: "Artificial Intelligence",
+    options: [
+      "Artificially Intelligent",
+      "Artificial Intelligence",
+      "Artificially Intelligence",
+      "Advanced Intelligence"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is Artificial Intelligence?",
+    answer: "Artificial Intelligence is a field that aims to develop intelligent machines",
+    options: [
+      "Artificial Intelligence is a field that aims to make humans more intelligent",
+      "Artificial Intelligence is a field that aims to improve the security",
+      "Artificial Intelligence is a field that aims to develop intelligent machines",
+      "Artificial Intelligence is a field that aims to mine the data"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who is the inventor of Artificial Intelligence?",
+    answer: "John McCarthy",
+    options: [
+      "Geoffrey Hinton",
+      "Andrew Ng",
+      "John McCarthy",
+      "Jürgen Schmidhuber"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is the branch of Artificial Intelligence?",
+    answer: "Machine Learning",
+    options: [
+      "Machine Learning",
+      "Cyber forensics",
+      "Full-Stack Developer",
+      "Network Design"
+    ]
+  },
+    {
+    numb: 5,
+    question: " What is the goal of Artificial Intelligence?",
+    answer: "To explain various sorts of intelligence",
+    options: [
+      "To solve artificial problems",
+      "To extract scientific causes",
+      "To explain various sorts of intelligence",
+      "To solve real-world problems"
+    ]
+  },
+
+
+    {
+    numb: 6,
+    question: "Which of the following is an application of Artificial Intelligence?",
+    answer: "Language understanding and problem-solving (Text analytics and NLP)",
+    options: [
+      "It helps to exploit vulnerabilities to secure the firm",
+      "Easy to create a website",
+      "Language understanding and problem-solving (Text analytics and NLP)",
+      "It helps to deploy applications on the cloud"
+    ]
+  },
+
+    {
+    numb: 7,
+    question: "In how many categories process of Artificial Intelligence is categorized?",
+    answer: "categorized into 3 categories",
+    options: [
+      "categorized into 5 categories",
+      "processes are categorized based on the input provided",
+      "categorized into 3 categories",
+      "process is not categorized"
+    ]
+  },
+  
+  {
+    numb: 8,
+    question: "Based on which of the following parameter Artificial Intelligence is categorized?",
+    answer: "Based on capabilities and functionally",
+    options: [
+      "Based on functionally only",
+      "Based on capabilities only",
+      "Based on capabilities and functionally",
+      "It is not categorized"
+    ]
+  },
+  
+  {
+    numb: 9,
+    question: " Which of the following is a component of Artificial Intelligence?",
+    answer: "Learning",
+    options: [
+      "Learning",
+      "Training",
+      "Designing",
+      "Puzzling"
+    ]
+  },
+  
+  {
+    numb: 10,
+    question: "What is the function of an Artificial Intelligence “Agent”?",
+    answer: "Mapping of precept sequence to an action",
+    options: [
+      "Mapping of goal sequence to an action",
+      "Work without the direct interference of the people",
+      "Mapping of precept sequence to an action",
+      "Mapping of environment sequence to an action"
+    ]
+  },
+];
